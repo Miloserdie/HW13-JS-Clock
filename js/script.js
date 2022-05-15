9e2eecf55b6278784a7211300b2fd5781a281bb9
Miloserdie <89547280+Miloserdie@users.noreply.github.com>
2022-05-15 11:58:37 +0300

    update

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ function Clock(e) {
 	this.element = e;
 	this.fullFormat = true;
 
-	Clock.prototype.render = function () {
-		if (this.fullFormat) {
-			this.element.innerHTML = new Date().toLocaleTimeString();
-		} else {
-			this.element.innerHTML = new Date().toLocaleTimeString().substring(0, 5);
-		}
-	}
-
-	Clock.prototype.interval = setInterval(() => this.render(), 1000);
-
-	Clock.prototype.changeFormat = function () {
-		this.fullFormat = !this.fullFormat;
-	}
+	this.interval = setInterval(() => this.render(), 1000);
 
 	this.element.addEventListener('click', () => this.changeFormat());
+}
+
+Clock.prototype.render = function () {
+	if (this.fullFormat) {
+		this.element.innerHTML = new Date().toLocaleTimeString();
+	} else {
+		this.element.innerHTML = new Date().toLocaleTimeString().substring(0, 5);
+	}
+}
+
+Clock.prototype.changeFormat = function () {
+	this.fullFormat = !this.fullFormat;
 }
 
 let clockOne = new Clock(document.querySelector('.first-clock'));
